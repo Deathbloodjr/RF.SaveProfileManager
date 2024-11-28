d052444b05099328af9a74f22f2f03a64e1193d8
Deathblood <101686464+Deathbloodjr@users.noreply.github.com>
2024-11-28 16:56:54 -0600

    Initial mod commit.
    Currently hardcoded to use a new profile named "Drum"

--- SaveProfileSwitcher/Logger.cs
+++ SaveProfileSwitcher/Logger.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ModTemplate
+namespace SaveProfileSwitcher
 {
     public enum LogType
     {
---
+++ SaveProfileSwitcher/Plugins/GetSavePathHook.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using Platform.Steam;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveProfileSwitcher.Plugins
+{
+    internal class GetSavePathHook
+    {
+        [HarmonyPatch(typeof(SteamSave))]
+        [HarmonyPatch(nameof(SteamSave.GetSavePath))]
+        [HarmonyPatch(MethodType.Normal)]
+        [HarmonyPostfix]
+        public static void SteamSave_GetSavePath_Postfix(SteamSave __instance, ref string __result)
+        {
+            //Plugin.Log.LogInfo(__result);
+
+            var split = __result.Split("\\");
+            split[split.Length - 2] = "Drum";
+            __result = Path.Combine(split);
+
+            Plugin.Log.LogInfo(__result);
+        }
+    }
+}
